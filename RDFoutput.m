82db556937c59950380cd037e1f90562c86e751c
Adi Rotem <Adi.Rotem@mail.huji.ac.il>
2016-01-19 17:58:11 +0200

    fixed bug - creating obj from file list

--- RDFoutput.m
+++ RDFoutput.m
@@ -51,7 +51,7 @@ classdef RDFoutput
                   clear MC2DLJs dataFileList;
                   
                   % get old RDF data if it exists
-                  if ~isempty(whos(obj.MC2DLJs(1,1).data,'RDFhisto'))
+                  
                         [Niso, Nrho] = size(obj.MC2DLJs);
                         [~,numOfBins] = size(obj.MC2DLJs(1,1).data.RDFbins);
                         obj.numOfBins = numOfBins;
@@ -59,15 +59,17 @@ classdef RDFoutput
                         obj.data.bins = zeros(Niso,Nrho,numOfBins);
                         for iso = 1:Niso
                             for rho = 1:Nrho
-                                obj.data.histo(iso,rho,:) = reshape(...
-                                    mean(obj.MC2DLJs(iso,rho).data.RDFhisto),...
-                                    [1,1,numOfBins]);
-                                obj.data.bins(iso,rho,:) = reshape(...
-                                    obj.MC2DLJs(iso,rho).data.RDFbins,...
-                                    [1,1,numOfBins]);
+                                if ~isempty(whos(obj.MC2DLJs(iso,rho).data,'RDFhisto'))
+                                    obj.data.histo(iso,rho,:) = reshape(...
+                                        mean(obj.MC2DLJs(iso,rho).data.RDFhisto),...
+                                        [1,1,numOfBins]);
+                                    obj.data.bins(iso,rho,:) = reshape(...
+                                        obj.MC2DLJs(iso,rho).data.RDFbins,...
+                                        [1,1,numOfBins]);
+                                end
                             end
                         end
-                  end
+                  
 
                   
               else
