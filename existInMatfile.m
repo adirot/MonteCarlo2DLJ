f6c04ae9e9fe4c380cf2fdb5bed58f1a5c5912ea
Adi Rotem <Adi.Rotem@mail.huji.ac.il>
2016-02-01 13:03:16 +0200

    matfile as input option

--- existInMatfile.m
+++ existInMatfile.m
@@ -1,4 +1,18 @@
-function exists = existInMatfile(fileName,varName)
+function exists = existInMatfile(fileNameOrMatfile,varName)
+    if isobject(fileNameOrMatfile)
+        try
+            fileName = fileNameOrMatfile.Properties.Source;
+        catch
+            error('first input must be file name or matfile object');
+        end
+    else
+        if ischar(fileNameOrMatfile)
+            fileName = fileNameOrMatfile;
+        else
+            error('first input must be file name or matfile object');
+        end
+    end
+    
     var = whos('-file',fileName);
     var = {var.name};
     exists = any(strcmp(var,varName));
