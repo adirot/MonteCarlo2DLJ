26e1f0ddb5514ea5e21f8a41a9414a3dbad509b9
Adi Rotem <Adi.Rotem@mail.huji.ac.il>
2016-07-20 10:51:40 +0300

    notation

--- minimal_save_rhoDist.m
+++ minimal_save_rhoDist.m
@@ -14,17 +14,17 @@ r = 0.1; m = 3; sq = 256; sqind = sqrt(sq); t = 2; simind = 9;
 %meanhistnumOfPartInSquare_inStep{tind,1} = meanhistnumOfCellsInSquare{tind,1,1,sqind};
 
 steps(1,1) = 1;
-[M, histxnumOfPartInSquare, histnumOfPartInSquare,...
+[M, histXnumOfPartInSquare, histnumOfPartInSquare,...
                numOfPartInSquare] =...
                M.calcRhoDistrib(sq,'coordsFromObj',true);
-sumhistnumOfPartInSquare = histxnumOfPartInSquare;
+sumhistnumOfPartInSquare = histXnumOfPartInSquare;
            
 totSec = 0;
 for i = 1:1000
     tic;
     steps(1,1+i) = steps(1,1) + i; 
     M = M.MonteCarlo(10,0,'save2data',false);
-    [M, histxnumOfPartInSquare, histnumOfPartInSquare,...
+    [M, histXnumOfPartInSquare, histnumOfPartInSquare,...
                numOfPartInSquare] =...
                M.calcRhoDistrib(sq,'coordsFromObj',true);
     
