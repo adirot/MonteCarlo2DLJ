38ba0b76ffeb1918742722f57eee545ee5bb259e
adirot <adi.rotem@mail.huji.ac.il>
2016-08-23 16:08:11 +0300

    bug

--- analysis/getSnapshots.m
+++ analysis/getSnapshots.m
@@ -1,9 +1,10 @@
 
-list = dir('N*T*rho0_1*m3*mat');
+list = dir('hcr/N*T0_0*mat');
 list = {list.name};
 
 for i = 1:length(list)
-    M = MC2DLJoutput(list{1,i});
+    M = MC2DLJoutput(['hcr/' list{1,i}]);
     M.showStep('last');
-   save(gcf,['snapshotN' num2srt(M.simulationParam.N) 'T' my_num2str(M.simulationParam.T) 'rho' my_num2str(M.simulationParam.rho) 'm' num2str(M.simulationParam.m) 'last.fig']);
+   saveas(gcf,['hcr/snapshotN' num2str(M.simulationParam.N) 'T' my_num2str(M.simulationParam.T) 'rho' my_num2str(M.simulationParam.rho) 'm' num2str(M.simulationParam.m) 'hcr_last.fig']);
+   disp(i);
 end
